9479450b6d87319a808ff21259091e4a2422eae6
Hannah Opara <oparanaomi123@gmail.com>
2022-06-19 08:13:33 +0100

    ignore files

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+class Post(models.Model):
+    Title = models.CharField(max_length=200)
+    Text = models.TextField()
+    Created_date =  models.DateTimeField()
+    Published_date = models.DateTimeField()
+
+    def _str_(self) -> str:
+        return self.name
