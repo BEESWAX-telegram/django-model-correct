eb3ab7a3091a3fb86c68e5753c20f656d2ac8948
Hannah--- <87046881+Ngozi-opara@users.noreply.github.com>
2022-06-25 15:43:23 +0100

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Post(models.Model):
-    Title = models.CharField(max_length=200)
-    Text = models.TextField()
-    Created_date =  models.DateTimeField()
-    Published_date = models.DateTimeField()
+    author = models.ForeignKey(User, related_name = 'author', on_delete = models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date =  models.DateTimeField(auth_now_add=True)
+    published_date = models.DateTimeField(auth_now_add=True)
 
-    def _str_(self) -> str:
-        return self.name
+    def _str_(self):
+        return self.title
